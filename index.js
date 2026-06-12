fc658b4afe67065eca409c99119483dc9c8297e2
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-06-12 17:28:15 +0500

    navbar creating

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const question = document.querySelector(".question");
-const answer = document.querySelector(".answer");
+// const question = document.querySelector(".question");
+// const answer = document.querySelector(".answer");
 
-question.addEventListener("click", () => {
-     if(question.textContent=== "+"){
-        question.textContent = "✖️"
-    }else{
-        question.textContent = "+"
-    }
-  if (answer.style.display === "none") {
-    answer.style.display = "block";
-  } else {
-    answer.style.display = "none";
-  }
+// question.addEventListener("click", () => {
+//      if(question.textContent=== "+"){
+//         question.textContent = "✖️"
+//     }else{
+//         question.textContent = "+"
+//     }
+//   if (answer.style.display === "none") {
+//     answer.style.display = "block";
+//   } else {
+//     answer.style.display = "none";
+//   }
  
-});
+// });
 
